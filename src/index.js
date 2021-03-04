3ad8f90e6e15c9816f64cacce6988453b51b0a22
Ritish <ritish.2818160@piet.co.in>
2021-03-04 20:02:38 +0530

    Initialized context API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import './index.css';
@@ -6,6 +6,7 @@ import App from './components/App';
 import combineReducers from './reducers';
 import thunk from "redux-thunk";
 
+// We have used thunk inbuilt library instead of below thunk
 // const thunk = ({dispatch, getState}) => (next) => (action) =>{
 //   if(typeof(action) == 'function'){
 //     action(dispatch);
@@ -15,12 +16,24 @@ import thunk from "redux-thunk";
 // };
 
 const store = createStore(combineReducers, applyMiddleware(thunk));
+export const storeContext = createContext();
+
+class Provider extends React.Component{
+  render(){
+    const {store} = this.props;
+    return(
+      <storeContext.Provider value = {store}>
+        {this.props.children}
+      </storeContext.Provider>
+    );
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App 
-      store = {store}
-    />
+    <Provider value={store}>
+      <App store = {store}/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
